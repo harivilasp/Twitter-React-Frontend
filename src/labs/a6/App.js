66b410bbf00dcdf5195d145c205ab3f289bf047f
harivilasp <harivilasp@gmail.com>
2023-02-17 21:10:44 -0500

    Started a

---
+++ src/labs/a6/App.js
@@ -0,0 +1,12 @@
+import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
+
+function App() {
+   return (
+      <div className="container">
+         <HelloWorld/>
+         <Labs/>
+      </div>
+   );
+}
+export default App;
