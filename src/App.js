c9abe01cc4ac330a87087ceef74e3ba20bcd0a17
harivilasp <harivilasp@gmail.com>
2023-03-11 17:51:35 -0500

    Previous update only

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Route, Routes} from "react-router"
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { faComment } from '@fortawesome/free-solid-svg-icons'
+// library.add(faComment)
 
 function App() {
   return (
