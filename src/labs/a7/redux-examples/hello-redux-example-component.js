24cd189ba292fc616e9087cdd522f2439ce16928
harivilasp <harivilasp@gmail.com>
2023-03-17 21:29:22 -0400

    basic compelted yet to start home component

--- src/labs/a7/redux-examples/hello-redux-example-component.js
+++ src/labs/a7/redux-examples/hello-redux-example-component.js
@@ -1,11 +1,11 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React from 'react';
+import {useSelector} from 'react-redux';
 
 const HelloReduxExampleComponent = () => {
- const message = useSelector((hello) => hello.message);
- return(
-   <h3>{message}</h3>
- );
-};
-
-export default HelloReduxExampleComponent;
+  const message = useSelector((state) => state.hello.message);
+  return(
+    <h1>{message}</h1>
+  );
+ };
+ 
+ export default HelloReduxExampleComponent;
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,12 +1,21 @@
+import HomeComponent from "./home";
 import React from "react";
 import ExploreComponent from "./explore";
-import NavigationSidebar from "./navigation-sidebar";
+import NavigationSidebar
+  from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
 import {Routes, Route} from "react-router";
-import HomeComponent from "./home";
+import whoReducer
+  from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
+import { configureStore } from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore({
+ reducer: {who: whoReducer, tuits: tuitsReducer}});
 
 function Tuiter() {
  return (
+  <Provider store={store}>
    <div className="row mt-2">
       <div className="col-2 col-md-2 col-lg-1 col-xl-2">
         <NavigationSidebar active="explore"/>
@@ -22,8 +31,8 @@ function Tuiter() {
      <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
         <WhoToFollowList/>
      </div>
-
    </div>
+  </Provider>
  );
 }
 
