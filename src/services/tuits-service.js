a8f24dc64810715d622c45590a4c7137c5198eca
harivilasp <harivilasp@gmail.com>
2023-04-05 12:02:39 -0400

    Changed envirionment variable name

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE_A9;
 // console.log("API_BASE "+API_BASE)
 // const API_BASE = "http://localhost:4000/api"
 // const API_BASE = "https://tuiter-node-app-cs5610-07-sp23.onrender.com/api";
