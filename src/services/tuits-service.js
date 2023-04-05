8aba49d686782d994de18e524d98820252a1f57a
harivilasp <harivilasp@gmail.com>
2023-04-05 12:14:32 -0400

    Changed engorionment variable

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE_A9;
 // console.log("API_BASE "+API_BASE)
 // const API_BASE = "http://localhost:4000/api"
-// const API_BASE = "https://tuiter-node-app-cs5610-07-sp23.onrender.com/api";
+// const API_BASE = "https://tuiter-node-app-cs5610-07-sp23-a9.onrender.com/api";
+
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
